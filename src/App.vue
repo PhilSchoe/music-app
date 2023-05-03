e6f3b12acda9271e9a90076434256f085ca2b49a
philippschoemacker <p.schoemacker@gmx.de>
2023-05-03 08:12:02 +0200

    Rname header back to AppHeader to avoid naming conflict

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Header -->
-  <header></header>
+  <app-header></app-header>
 
   <!-- Introduction -->
   <section class="mb-8 py-20 text-white text-center relative">
@@ -271,13 +271,13 @@
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
+import AppHeader from '@/components/Header.vue'
 import Auth from '@/components/Auth.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
+    AppHeader,
     Auth
   }
 }
